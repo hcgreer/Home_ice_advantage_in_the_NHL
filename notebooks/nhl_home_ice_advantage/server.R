0603cfe84321aae5bc3390d2f9343852808d8146
hcgreer <hcgreer07@gmail.com>
2023-01-19 22:55:42 -0600

    Finished shiny app

--- notebooks/nhl_home_ice_advantage/server.R
+++ notebooks/nhl_home_ice_advantage/server.R
@@ -22,31 +22,103 @@ shinyServer(function(input, output, session){
              filter(team %in% input$team, season == input$season))
       })
   
+  elos_filtered <- reactive({
+    if(input$season == 'All') {
+      return(yearly_elo %>% 
+               filter(team %in% input$team))
+    }
+    return(elos %>% 
+             filter(team %in% input$team, season == input$season))
+  })
+  
   output$wins <- renderPlot({
     seaons_filtered() %>%
-      filter(home_away %in% c('home_win', 'away_win')) %>% 
-      ggplot(aes(x = team, y = count/10, fill = home_away)) +
-        geom_bar(stat = 'identity', position = 'dodge', width = .4)
+      filter(home_away %in% c('home_win', 'home_loss')) %>% 
+      ggplot(aes(x = team, y = count, fill = home_away)) +
+        geom_bar(stat = 'identity', position = 'fill', width = .4) +
+        scale_fill_manual(
+          values = c('steelblue1', 'steelblue3'), 
+          labels = c('Loss', 'Win'), 
+          guide = guide_legend(reverse=TRUE)
+          ) +
+        xlab('Team(s)') +
+        ylab('Home Win Percentage') +
+        geom_hline(yintercept = .5)
   })
   
   output$losses <- renderPlot({
     seaons_filtered() %>%
-      filter(home_away %in% c('home_loss', 'away_loss')) %>% 
-      ggplot(aes(x = team, y = count/10, fill = home_away)) +
-      geom_bar(stat = 'identity', position = 'dodge', width = .4)
+      filter(home_away %in% c('away_win', 'away_loss')) %>% 
+      ggplot(aes(x = team, y = count, fill = home_away)) +
+      geom_bar(stat = 'identity', position = 'fill', width = .4) +
+      scale_fill_manual(
+        values = c('steelblue1', 'steelblue3'), 
+        labels = c('Loss', 'Win'), 
+        guide = guide_legend(reverse=TRUE)
+      ) +
+      xlab('Team(s)') +
+      ylab('Away Win Percentage') +
+      geom_hline(yintercept = .5)
   })
-  
   output$goals <- renderPlot({
     seaons_filtered() %>%
       filter(home_away %in% c('home_goals', 'away_goals')) %>% 
-      ggplot(aes(x = team, y = count/10, fill = home_away)) +
-      geom_bar(stat = 'identity', position = 'dodge', width = .4)
+      ggplot(aes(x = team, y = count, fill = home_away)) +
+      geom_bar(stat = 'identity', position = 'dodge', width = .4) + 
+      scale_fill_manual(
+        values = c('steelblue1', 'steelblue3'), 
+        labels = c('Away', 'Home'), 
+        guide = guide_legend(reverse=TRUE)
+      ) +
+      xlab('Team(s)') +
+      ylab('Number of Goals')
   })
   
   output$shots <- renderPlot({
     seaons_filtered() %>%
       filter(home_away %in% c('home_shots', 'away_shots')) %>% 
-      ggplot(aes(x = team, y = count/10, fill = home_away)) +
-      geom_bar(stat = 'identity', position = 'dodge', width = .4)
+      ggplot(aes(x = team, y = count, fill = home_away)) +
+      geom_bar(stat = 'identity', position = 'dodge', width = .4) + 
+      scale_fill_manual(
+        values = c('steelblue1', 'steelblue3'), 
+        labels = c('Away', 'Home'), 
+        guide = guide_legend(reverse=TRUE)
+      ) +
+      xlab('Team(s)') +
+      ylab('Number of Shots')
   })
-})
+  
+  output$elo <- renderPlot({
+    if (input$season == 'All'){
+      return( elos_filtered() %>% 
+        ggplot(aes(x = season, y = elos, group = team)) + 
+          geom_line(aes(color = team)) + 
+          theme(axis.text.x = element_blank()))
+    }
+    return(elos_filtered() %>% 
+      ggplot(aes(x = date, y = elo, group = team)) +
+        geom_line(aes(color = team)))
+  })
+  
+  output$end_elo <- renderDataTable({
+    if (input$season == 'All'){
+      return(end_elos %>% 
+               arrange(desc(elos)))
+    }
+    return(end_elos %>% 
+             filter(season == input$season) %>% 
+             select(team, elos) %>% 
+             arrange(desc(elos)))
+  })
+  
+  output$standings <- renderDataTable({
+    if (input$season == 'All'){
+      return(standings %>% 
+               arrange(desc(points)))
+    }
+    return(standings %>% 
+             filter(season == input$season) %>% 
+             select(team, points) %>% 
+             arrange(desc(points)))
+  })
+  })
